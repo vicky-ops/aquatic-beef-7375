4efbdec1818604a0714c8f4277101db2081be419
kaushik kumar <kaushikkumar7070@gmail.com>
2023-01-20 13:33:53 +0530

    day3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Laptop from './Products/Laptop/Laptop';
 
 function App() {
   return (
     <div className="App">
-      
+      <Laptop/>
     </div>
   );
 }
