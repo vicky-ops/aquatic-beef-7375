4a15fa3024f58722ab32ea2a72d7eeca3c44d306
kaushik kumar <kaushikkumar7070@gmail.com>
2023-01-20 13:30:36 +0530

    day3

--- src/Products/Laptop/Laptop.tsx
+++ src/Products/Laptop/Laptop.tsx
@@ -1,28 +1,120 @@
 import React, { useEffect, useState } from "react";
-import { getTodos } from "./api";
+
+import { getTodos, htl, lth, hP, APple, DEll } from "./api";
 import Card from "./Card";
 import { Product } from "./constant";
-const Laptop=()=>{
-    const [laptop,setLaptop] = useState<Product []>([])
+import axios, { AxiosResponse } from "axios"
 
-    useEffect(()=>{
-        getTodos().then((res)=>{
-           
+const Laptop = () => {
+    const [laptop, setLaptop] = useState<Product[]>([])
+
+    const [visible, setVisible] = useState(15)
+
+    const showMore = () => {
+        setVisible((prev) => prev + 15)
+    }
+    // const low=()=>{
+    //     lth().then((res)=>{
+    //         setLaptop(res)
+    //     })
+    // }
+    // const high=()=>{
+    //     htl().then((res)=>{
+    //         setLaptop(res)
+    //     })
+    // }
+    const hp = () => {
+        hP().then((res) => {
             setLaptop(res)
-            console.log(res);
-            
         })
-    },[])
+    }
+    const apple = () => {
+        APple().then((res) => {
+            setLaptop(res)
+        })
+    }
+    const dell = () => {
+        DEll().then((res) => {
+            setLaptop(res)
+        })
+    }
 
-    return <div>
-    <h1>laptop</h1>
- 
-    {laptop?.map((el)=>{
-    return <Card key={el.id} {...el}  />
-})}
+    // function HandleChange(e: { target: { value: string; }; }){
+    //     if(e.target.value==="asc"){
+    //         htl().then((res)=>{
+    //             setLaptop(res)
+    //         })
+    //     }else if(e.target.value==="dsc"){
+    //         lth().then((res)=>{
+    //             setLaptop(res)
+    //         })
+    //     }else if(e.target.value==="all"){
+    //         getTodos().then((res)=>{
+    //             setLaptop(res)
+    //         })
+    //     }
 
-    </div>
-       
-    
-}
+    const HandleChange = (e:React.ChangeEvent<HTMLSelectElement>) => {
+        if (e.target.value === "asc") {
+            htl().then((res) => {
+                setLaptop(res)
+            })
+        } else if (e.target.value === "dsc") {
+            lth().then((res) => {
+                setLaptop(res)
+            })
+        } else if (e.target.value === "all") {
+            getTodos().then((res) => {
+                setLaptop(res)
+            })
+        }
+
+
+
+        useEffect(() => {
+            getTodos().then((res) => {
+
+                setLaptop(res)
+                console.log(res);
+
+            })
+        }, [])
+
+        return (<div>
+            <h1>laptop</h1>
+
+            <div>
+                {/* <button onClick={low} >low to high</button>
+ <button  onClick={high} >high to low</button> */}
+                <h3>Filter</h3>
+                <button onClick={hp}  >HP</button>
+                <button onClick={apple} >Apple</button>
+                <button onClick={dell} >Dell</button></div>
+
+           <div>
+                <select onChange={HandleChange}  > Sort by: All
+                    <option value="all">Sort by: All</option>
+                    <option value="asc">Price (Low to High)</option>
+                    <option value="dsc">Price (High to Low)</option>
+                </select>
+            </div>  
+
+
+
+
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }} >{laptop?.slice(0, visible).map((el) => {
+                return <Card key={el.id} {...el} />
+            })}</div>
+            <button onClick={showMore} >Show More</button>
+
+
+
+
+        </div>
+
+)
+        }
+    }
+
 export default Laptop
+
--- src/Products/Laptop/api.ts
+++ src/Products/Laptop/api.ts
@@ -4,6 +4,26 @@ export const getTodos = async ()=>{
     let res: AxiosResponse<Product[]>  = await axios.get("https://bharatmart.onrender.com/laptop")
     return res.data
 }
+export const lth = async()=>{
+    let res: AxiosResponse<Product[]>  = await axios.get("https://bharatmart.onrender.com/laptop/?_sort=price&_order=asc")
+    return res.data
+}
+export const htl = async()=>{
+    let res: AxiosResponse<Product[]>  = await axios.get("https://bharatmart.onrender.com/laptop/?_sort=price&_order=desc")
+    return res.data
+}
+export const hP = async()=>{
+    let res: AxiosResponse<Product[]>  = await axios.get("https://bharatmart.onrender.com/laptop/?brand=HP")
+    return res.data
+}
+export const APple = async()=>{
+    let res: AxiosResponse<Product[]>  = await axios.get("https://bharatmart.onrender.com/laptop/?brand=Apple")
+    return res.data
+}
+export const DEll = async()=>{
+    let res: AxiosResponse<Product[]>  = await axios.get("https://bharatmart.onrender.com/laptop/?brand=Dell")
+    return res.data
+}
 // https://bharatmart.onrender.com/laptop
 
 // https://bharatmart.onrender.com/phones
