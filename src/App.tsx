d63984dcc47bf406ab51892df0bfa0eb4f34ce85
Vicky-ops <workmailvicky101@gmail.com>
2023-01-22 17:38:04 +0530

    --added login hard-coded login functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 import './App.css';
 import Laptop from './Products/Laptop/Laptop';
 
+import Login from './component/Login';
 
 
 function App() {
   return (
     <div className="App">
     <Laptop/>
+    <Login/>
     </div>
   );
 }
