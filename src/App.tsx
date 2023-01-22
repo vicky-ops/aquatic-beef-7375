4de3aeeaf9650c77305e9fe6443fe1032dbd9eb7
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-01-22 19:43:51 +0530

    final update

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@
 import React from 'react';
 
 import './App.css';
-import Cart from './component/Cart/Cart';
-import Ladies from './component/Ladies/Ladies';
-import Cartpage from './pages/Cartpage';
-import Industrialpage from './pages/Industrialpage';
-import LadiesPage from './pages/LadiesPage';
-import Shome from './pages/Shome';
+
 
 
 
@@ -16,7 +11,7 @@ import Shome from './pages/Shome';
 function App() {
   return (
     <div className="App">
-<Shome/>
+
 
     </div>
   );
